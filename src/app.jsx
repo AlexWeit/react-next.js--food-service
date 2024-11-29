3b6b8ba2988a199816a577ee313cf1a5431722b8
Alexandra Veyt <alexandra.pasyuta@gmail.com>
2024-11-29 16:31:59 +0300

    hw-7: move Contexts from Layout to App

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,17 @@
 import './app.css'
 import { Layout } from "./components/layout/layout";
 import { RestaurantsPage } from "./components/restaurantsPage/restaurantsPage";
+import {ThemeContextProvider} from "./components/theme-context/theme-context-provider";
+import {AuthContextProvider} from "./components/auth-context/auth-context-provider";
 
 export function App() {
     return (
-        <Layout>
-            <RestaurantsPage />
-        </Layout>
+        <AuthContextProvider>
+            <ThemeContextProvider>
+                <Layout>
+                    <RestaurantsPage />
+                </Layout>
+            </ThemeContextProvider>
+        </AuthContextProvider>
     );
 }
--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,20 +1,14 @@
 import {ScrollProgressBar} from "../scroll-progress-bar/scroll-progress-bar";
 import {Header} from "../header/header";
 import {Footer} from "../footer/footer";
-import { AuthContextProvider } from "../auth-context/auth-context-provider";
-import { ThemeContextProvider } from "../theme-context/theme-context-provider";
 
 export const Layout = ({ children }) => {
     return (
-        <AuthContextProvider>
-            <ThemeContextProvider>
-                <div>
-                    <ScrollProgressBar />
-                    <Header />
-                    {children}
-                    <Footer />
-                </div>
-            </ThemeContextProvider>
-        </AuthContextProvider>
+        <div>
+            <ScrollProgressBar />
+            <Header />
+            {children}
+            <Footer />
+        </div>
     )
 };
