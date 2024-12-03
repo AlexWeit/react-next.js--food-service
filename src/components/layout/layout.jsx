ed71d50723088eed2bdcfd91cb861c0065915222
Alexandra Veyt <alexandra.pasyuta@gmail.com>
2024-12-03 14:07:52 +0300

    hw-8: fix footer style

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -4,7 +4,7 @@ import {Footer} from "../footer/footer";
 
 export const Layout = ({ children }) => {
     return (
-        <div>
+        <div className="mainWrapper">
             <ScrollProgressBar />
             <Header />
             {children}
