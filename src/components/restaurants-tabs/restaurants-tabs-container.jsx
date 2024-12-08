f69b7eeedc899f25c51319f69fb9118f18a1b9b2
Alexandra Veyt <alexandra.pasyuta@gmail.com>
2024-12-08 20:05:47 +0300

    hw-9: add server-api, & restaurants-tabs component

---
+++ src/components/restaurants-tabs/restaurants-tabs-container.jsx
@@ -0,0 +1,15 @@
+import {useSelector} from "react-redux";
+import {selectRestaurantsIds} from "../../redux/entities/restaurants/restaurants-slice";
+import {RestaurantsTabs} from "../restaurants-tabs/restaurants-tabs";
+
+export const RestaurantsTabsContainer = () => {
+    const restaurantsIds = useSelector(selectRestaurantsIds);
+
+    if (!restaurantsIds.length) {
+        return null;
+    }
+
+    return (
+        <RestaurantsTabs restaurantsIds={restaurantsIds} />
+    )
+};
